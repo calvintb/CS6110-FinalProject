a79789b74b550179c3e4f781af7a4ac73fad966c
SarahNGardner <sarahngardner@gmail.com>
2024-12-05 12:53:02 -0700

    Update Destination.py

--- Destination.py
+++ Destination.py
@@ -3,6 +3,7 @@ from Intersection import Intersection
 class Destination(Intersection):
     def __init__(self, label=None):
         super().__init__(label)
+        self.is_destination = True
 
 
     def __str__(self):
